b30f3af4cf5da2ff22e1388f1d82d0c4d789796a
Gummadi Vamsidhar Reddy <vamsidharr04@gmail.com>
2025-06-30 12:56:31 +0530

    changed path to run scripts

--- scripts/control.sh
+++ scripts/control.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+fpath=$(dirname "$(realpath $0)")
+
 case "$1" in
-	"screen") $HOME/final/scripts/scrn.sh "$2" ;;
-	"wall") $HOME/final/scripts/random_wallpaper_picker.sh ;;
-	"check") $HOME/final/scripts/check.sh ;;
-	"sd") $HOME/final/scripts/sd.sh ;;
+	"screen") $fpath/scrn.sh "$2" ;;
+	"wall") $fpath/random_wallpaper_picker.sh ;;
+	"check") $fpath/check.sh ;;
+	"sd") $fpath/sd.sh ;;
 esac
